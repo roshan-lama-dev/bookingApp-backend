9d25274b5e4187ce7fdc415763b332c0b6e1ba5c
Havochalo <officialmail85@gmail.com>
2023-04-02 23:00:09 +1000

    Hotel routes completed

--- src/routers/hotelsRouter.js
+++ src/routers/hotelsRouter.js
@@ -14,8 +14,48 @@ router.post("/", async (req, res) => {
 });
 
 // UPDATE
+
+router.put("/:id", async (req, res) => {
+  try {
+    const updateHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+
+      { new: true }
+    );
+    res.status(200).json(updateHotel);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 // DELETE
+router.delete("/:id", async (req, res) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).json("Hotel has been Deleted");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 // GET
+router.get("/:id", async (req, res) => {
+  try {
+    const singleHotel = await Hotel.findById(req.params.id);
+    res.status(200).json(singleHotel);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 // GET ALL
+router.get("/", async (req, res) => {
+  try {
+    const allHotel = await Hotel.find();
+    res.status(200).json(allHotel);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 export default router;
